7cc4fd0a7ab64256981f1a11ae675959d5b838ad
truongmanhdat07 <truongmanhdat061@gmail.com>
2026-05-13 19:51:06 +0700

    Add files via upload

---
+++ F-Thao Tac Tren Tep Tin/F1/F1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	char tenfile[100];
+	printf("Vui long nhap ten file can doc :");
+	fflush(stdin);
+	gets(tenfile);
+	FILE *f=fopen(tenfile,"r");
+	if(f==NULL){
+		printf("\nKhong the doc duoc file !");
+		return 1;
+	}
+	printf("\nNoi dung cua file %s la :\n\n",tenfile);
+	char temp[1000];
+	while( fgets ( temp , sizeof(temp) , f ) ){
+		printf("%s",temp);
+	}
+	printf("\n\nDa doc het file %s",tenfile);
+	fclose(f);
+	return 0;
+}
